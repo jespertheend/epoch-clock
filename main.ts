fbae288765d47a6ab9a65d4baa1150c9a3ebec6a
Jesper van den Ende <jespertheend@gmail.com>
2023-11-12 17:12:27 +0100

    Revert "favicon test"
    This reverts commit 80d85bbfb3ad5f1b5e2aa33ffdcb7cad4ee60ee6.

--- main.ts
+++ main.ts
@@ -1,16 +1,6 @@
 import { serveDir } from "https://deno.land/std@0.206.0/http/file_server.ts";
-import { generateFavicon } from "./src/favicon.ts";
 
 Deno.serve(async (request) => {
-	const url = new URL(request.url);
-	if (url.pathname == "/favicon.ico") {
-		const image = generateFavicon();
-		return new Response(image, {
-			headers: {
-				"Content-Type": "image/png",
-			},
-		});
-	}
 	return await serveDir(request, {
 		showDirListing: true,
 		fsRoot: "static",
